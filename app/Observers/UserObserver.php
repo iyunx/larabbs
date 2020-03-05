c9faf8d4f8f094d832d638920da4b081535c29a1
张浩刚 <iyunx@qq.com>
2020-03-05 12:04:00 +0000

    联动删除

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -14,8 +15,9 @@ class UserObserver
         //
     }
 
-    public function updating(User $user)
+    public function deleted(User $user)
     {
-        //
+        DB::table('topics')->where('user_id', $user->id)->delete();
+        DB::table('replies')->where('user_id', $user->id)->delete();
     }
 }
